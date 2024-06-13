a53e4346e8d1cb30b233ab1fe1a7a7569e66a1bf
Ryan <95608156+SmallHedge@users.noreply.github.com>
2024-06-13 16:49:10 +1200

    Update ParallaxManagerEditor.cs

--- ParallaxManagerEditor.cs
+++ ParallaxManagerEditor.cs
@@ -1,81 +1,24 @@
 //Author: Small Hedge Games
-//Date: 13/06/2024
+//Date: 21/04/2024
 
+#if UNITY_EDITOR
 using UnityEngine;
-using System;
+using UnityEditor;
 
-public class ParallaxManager : MonoBehaviour
+[CustomEditor(typeof(ParallaxManager))]
+public class ParallaxManagerEditor : Editor
 {
-    [SerializeField] private Background[] backgrounds;
-    [SerializeField] private bool isBackground;
-    [SerializeField] private bool changeAnchorsOnStart;
-    [SerializeField] private float heightMultiplier;
-    [SerializeField] private Transform anchor;
-
-    public Transform gameCamera;
-    public static ParallaxManager instance;
-    private Vector3 anchorPosition;
-
     private void Awake()
     {
-        instance = this;
+        if (!((ParallaxManager)target).gameCamera) ((ParallaxManager)target).gameCamera = Camera.main.transform;
     }
 
-    private void Start()
+    public override void OnInspectorGUI()
     {
-        if (!anchor) anchorPosition = gameCamera.position;
-
-        if(changeAnchorsOnStart)
-            for (int i = 0; i < backgrounds.Length; i++)
-                backgrounds[i].anchor = backgrounds[i].sprite.position;
-    }
-
-    private void Update()
-    {
-        for (int i = 0; i < backgrounds.Length; i++)
-        {
-            if (anchor) anchorPosition = anchor.position;
-            float adjustedIntensity = backgrounds[i].intensity;
-            if (isBackground) --adjustedIntensity;
-            backgrounds[i].sprite.position = (Vector2)anchorPosition + backgrounds[i].anchor + new Vector2(-adjustedIntensity, adjustedIntensity * heightMultiplier) * (gameCamera.position - anchorPosition);
-        }
-    }
-
-    public void ChangeAnchor(int index, Vector2 anchor)
-    {
-        backgrounds[index].anchor = anchor;
-    }
-
-    public void ResetBackgrounds()
-    {
-        Array.Resize(ref backgrounds, transform.childCount);
-        for (int i = 0; i < backgrounds.Length; i++)
-        {
-            if (!backgrounds[i].sprite) 
-                backgrounds[i].intensity = GetIntensity(i);
-
-            backgrounds[i].sprite = transform.GetChild(i);
-            backgrounds[i].name = backgrounds[i].sprite.name;
-        }
-    }
-
-    public void ResetIntensities()
-    {
-        for (int i = 0; i < backgrounds.Length; ++i)
-            backgrounds[i].intensity = (float)(i + 1) / (backgrounds.Length + 1);
-    }
-
-    public float GetIntensity(int index)
-    {
-        return backgrounds[index].intensity;
+        base.OnInspectorGUI();
+        ParallaxManager manager = (ParallaxManager)target;
+        manager.ResetBackgrounds();
+        if (GUILayout.Button("Set Intensities")) manager.ResetIntensities();
     }
 }
-
-[Serializable]
-public struct Background
-{
-    [HideInInspector] public string name;
-    [Range(0, 1)] public float intensity;
-    public Transform sprite;
-    public Vector2 anchor;
-}
+#endif
